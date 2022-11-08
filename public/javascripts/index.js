7ebb6a8c766a0216649c34226ec0d467eee98216
Jackson Kearl <jackson.kearl@outlook.com>
2022-11-07 20:55:01 -0800

    Migrate to Cloudflare

--- public/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-const runFingerprint = () => {
-    $('loading').innerText = "Loading fingerprint..."
-    setTimeout(() =>
-        hashFingerprint(hashedFingerprint => {
-            $('loading').innerText = "Enter Your Chatroom"
-            window.location.href = '/room/' + hash(hashedFingerprint)
-        }), 30)
-}
