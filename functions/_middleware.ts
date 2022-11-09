941f0b3f7b386310f390c5439180a8ec5f551d66
Jackson Kearl <jackson.kearl@outlook.com>
2022-11-09 11:44:02 -0800

    Favicon add

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -56,6 +56,7 @@ const rewriter = (name: string, fingerprint: string, signedIn: boolean) =>
 					'<meta http-equiv="X-UA-Compatible" content="IE=edge">',
 					'<meta name="viewport" content="width=device-width, initial-scale=1.0">',
 					'<link rel="stylesheet" type="text/css" href="/index.css" />',
+					'<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>',
 					'<title>FingrPrintr</title>',
 				)
 			},
