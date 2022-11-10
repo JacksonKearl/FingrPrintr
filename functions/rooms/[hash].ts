dacfdd940229f39d2de03a4cd1ccb5bbf54fa4f8
Jackson Kearl <jackson.kearl@outlook.com>
2022-11-09 17:54:57 -0800

    Fix field naming bg

--- functions/rooms/[hash].ts
+++ functions/rooms/[hash].ts
@@ -82,76 +82,72 @@ export const onRequestGet: PagesFunction<Env, 'hash', Data> = async ({
 
 	const rewriter = new HTMLRewriter().on('main#chats', {
 		async element(element) {
-			try {
-				const chats = await KVChat(roomId, env).get()
+			const chats = await KVChat(roomId, env).get()
 
-				if (isOurRoom) {
-					const welcomePhrase = [
-						'Welcome',
-						'Hello',
-						'Greetings',
-						'Bonjour',
-						'Hola',
-						'Ciao',
-						'Howdy',
-						'Konnichiwa',
-						'Look what the cat dragged in...',
-						'Hiya',
-						'Aloha',
-						'Salam',
-					]
+			if (isOurRoom) {
+				const welcomePhrase = [
+					'Welcome',
+					'Hello',
+					'Greetings',
+					'Bonjour',
+					'Hola',
+					'Ciao',
+					'Howdy',
+					'Konnichiwa',
+					'Look what the cat dragged in...',
+					'Hiya',
+					'Aloha',
+					'Salam',
+				]
 
-					const welcomeText =
-						selectRandom(welcomePhrase) + ' ' + data.name + '!'
+				const welcomeText =
+					selectRandom(welcomePhrase) + ' ' + data.name + '!'
 
-					if (
-						chats.length < 20 &&
-						!chats.some(
-							(c) =>
-								c.author === 'root' &&
-								c.message.includes(data.name),
-						)
-					) {
-						chats.push({
-							author: 'root',
-							date: Date.now(),
-							message: welcomeText,
-						})
+				if (
+					chats.length < 20 &&
+					!chats.some(
+						(c) =>
+							c.author === 'root' &&
+							c.message?.includes(data.name),
+					)
+				) {
+					chats.push({
+						author: 'root',
+						date: Date.now(),
+						message: welcomeText,
+					})
 
-						KVChat(roomId, env).post('root', welcomeText)
-					}
+					KVChat(roomId, env).post('root', welcomeText)
 				}
-				if (chats.length) {
-					let lastAuthor: string | undefined
-					for (const chat of chats) {
-						const isOurChat = chat.author === data.name && isOurRoom
-						const dateStamp = new Date(chat.date).toLocaleString()
-						const creator = isOurChat ? 'ours' : 'theirs'
+			}
+			if (chats.length) {
+				let lastAuthor: string | undefined
+				for (const chat of chats) {
+					const isOurChat = chat.author === data.name && isOurRoom
+					const dateStamp = new Date(chat.date).toLocaleString()
+					const creator = isOurChat ? 'ours' : 'theirs'
 
-						element.append(
-							`<div title="${dateStamp}" class="chat ${creator}">`,
-							{ html: true },
-						)
-						if (lastAuthor !== chat.author) {
-							lastAuthor = chat.author
-							element.append('<span class="author">', {
-								html: true,
-							})
-							element.append(chat.author, { html: false })
-							element.append('</span>', { html: true })
-						}
-						element.append('<span class="message">', { html: true })
-						element.append(chat.message, { html: false })
+					element.append(
+						`<div title="${dateStamp}" class="chat ${creator}">`,
+						{ html: true },
+					)
+					if (lastAuthor !== chat.author) {
+						lastAuthor = chat.author
+						element.append('<span class="author">', {
+							html: true,
+						})
+						element.append(chat.author, { html: false })
 						element.append('</span>', { html: true })
-						element.append('</div>', { html: true })
 					}
-				} else {
-					element.append('<div class="chat">', { html: true })
-					element.append('No one here yet!')
+					element.append('<span class="message">', { html: true })
+					element.append(chat.message, { html: false })
+					element.append('</span>', { html: true })
 					element.append('</div>', { html: true })
 				}
-			} catch (e) {
-				element.append(e.message)
+			} else {
+				element.append('<div class="chat">', { html: true })
+				element.append('No one here yet!')
+				element.append('</div>', { html: true })
 			}
 		},
 	})
